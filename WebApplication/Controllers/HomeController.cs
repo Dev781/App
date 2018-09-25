f2f982ac2072c75a2d8d71dab708ea0ecc72e9b5
Dev781 <43578220+Dev781@users.noreply.github.com>
2018-09-25 16:56:51 +0200

    Init

---
+++ WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System.Diagnostics;
+using WebApplication.Models;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.NodeServices;
+
+namespace WebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IViewRenderService _viewRenderService;
+        private PdfTemplateModel TestModel { get; set; }
+        public HomeController(IViewRenderService viewRenderService)
+        {
+            _viewRenderService = viewRenderService;
+            TestModel = new PdfTemplateModel
+            {
+                Age = 45,
+                Country = "United Kingdom",
+                Name = "John Smith"
+            };
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> DownloadPdf([FromServices] INodeServices nodeServices)
+        {
+            var html = await _viewRenderService.RenderToStringAsync("Home/PdfTemplate", TestModel);
+            var result = await nodeServices.InvokeAsync<byte[]>("./pdf", html);
+
+            return File(result, "application/pdf", "myFile.pdf");
+        }
+
+        [HttpGet]
+        public IActionResult PdfTemplate()
+        {
+            return View("~/Views/Home/PdfTemplate.cshtml", TestModel);
+        }
+    }
+}
---
+++ WebApplication/pdf.js
@@ -0,0 +1,22 @@
+﻿module.exports = function (callback, data) {
+    var jsreport = require('jsreport-core')();
+
+    jsreport.init().then(function () {
+        return jsreport.render({
+            template: {
+                content: data,
+                engine: 'jsrender',
+                recipe: 'phantom-pdf',
+                phantom: {
+                    format: 'A4',
+                    margin: { top: 0, right: 0, bottom: 0, left: 0 },
+                    orientation: "landscape"
+                }
+            }
+        }).then(function (resp) {
+            callback(null, resp.content.toJSON().data);
+        });
+    }).catch(function (e) {
+        callback(e, null);
+    })
+};
